2f11a427b61bf56ff9fd4fb2fd2faf17bfe85554
Ankush263 <ankushbanik263@gmail.com>
2023-05-26 19:36:56 +0530

    update

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const Cloud = require('@google-cloud/storage')
 const path = require('path')
 // const serviceKey = path.join(__dirname, './keys.json')
-const serviceKey = path.join(__dirname, '/etc/secrets/fow-farm-16f1be2a6edc.json')
+const serviceKey = path.join('/etc/secrets/fow-farm-16f1be2a6edc.json')
 
 const { Storage } = Cloud
 const storage = new Storage({
