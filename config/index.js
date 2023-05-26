e9fa1bb38976581b6e50286ec9e70a80d56290f0
Ankush263 <ankushbanik263@gmail.com>
2023-05-26 18:02:34 +0530

    update

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const Cloud = require('@google-cloud/storage')
+const path = require('path')
+// const serviceKey = path.join(__dirname, './keys.json')
+const serviceKey = path.join(__dirname, './fow-farm-16f1be2a6edc.json')
+
+const { Storage } = Cloud
+const storage = new Storage({
+  keyFilename: serviceKey,
+  projectId: 'fow-farm',
+})
+
+module.exports = storage
